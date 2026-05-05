562fa3e724e2b98ad947735d48ad542fde9fdb86
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-05 11:03:05 +0200

    add islower

---
+++ python_fundamentals/functions_modules/islower.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def islower(s):
+    """
+    Return True if all cased characters
+    in the string are lowercase and there
+    is at least one cased character,
+    False otherwise.
+
+    >>> islower('abc')
+    True
+    >>> islower('Abc')
+    False
+    >>> islower('123')
+    False
+    >>> islower('')
+    False
+    """
+    return s.islower()
