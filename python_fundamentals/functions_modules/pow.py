d31d68fafb5e15abbfda73d988be2e7b3090fc8e
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-05 11:58:54 +0200

    add pow

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,3 +1,9 @@
 def pow(a, b):
     """Returns a to the power of b."""
-    return a ** b
+    if b < 0:
+        a = 1 / a
+        b = -b
+    result = 1
+    for _ in range(b):
+        result *= a
+    return result
