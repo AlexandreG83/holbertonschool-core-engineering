f25761bcd457888591eb19f1719645d581f00aff
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-05 09:53:54 +0200

    add outputs

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,12 +1,6 @@
-# print: 
-# Language: Python
-# Version: 3
-# Pi approx: 3.14
-# Computation valid: True
-
 print("Language: Python")
 print("Version: 3")
-pi_approx = 3.14
-print(f"Pi approx: {pi_approx}")
+pi_approx = 3.14159
+print(f"Pi approx: {pi_approx:.2f}")
 computation_valid = True
 print(f"Computation valid: {computation_valid}")
