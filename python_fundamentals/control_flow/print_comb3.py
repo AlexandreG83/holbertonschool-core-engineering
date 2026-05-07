23a52cace6c53da2f9e3b659afd108e489561a2c
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-07 15:34:14 +0200

    add file

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print(f"{i}{j}")
-        else:
-            print(f"{i}{j}", end=", ")
+
+first = True
+for i in range(0, 10):
+    for j in range(0, 10):
+        if j > i:
+            if not first:
+                print(", ", end="")
+            print("{}{}".format(i, j), end="")
+            first = False
+print()
