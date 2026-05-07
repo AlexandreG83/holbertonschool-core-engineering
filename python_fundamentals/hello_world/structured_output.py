0dad1c1d2c9a98fe3b3a9d0e00b1bdf0b80f57f7
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-07 09:44:14 +0200

    Fix computation validity check in structured_output.py

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+
 print("Language: Python")
 print("Version: 3")
 
 pi_approx = 3.14159
 print("Pi approx: {:.2f}".format(pi_approx))
 
-computation_valid = True
+computation_valid = (5 > 3)
 print("Computation valid: {}".format(computation_valid))
