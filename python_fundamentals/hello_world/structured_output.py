28f3afdd787f8a9c44cef7bb35b7e063270bfc28
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-07 11:33:49 +0200

    add

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-language = 'Python'
+language = "Python"
 version = 3
-pi_approx = 3.14159
-computation_valid = 1 == 1
+pi = 3.141592653589793
+is_valid = 1 == 1
 
 print(f"Language: {language}")
 print(f"Version: {version}")
-print(f"Pi approx: {pi_approx:.2f}")
-print(f"Computation valid: {computation_valid}")
+print(f"Pi approx: {pi:.2f}")
+print(f"Computation valid: {is_valid}")
