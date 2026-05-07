f1a4ecefdf4cf6e65db9d2dec968c8b6daa76fc7
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-07 15:04:05 +0200

    add file

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 for i in range(26):
-    if i == 4 or i == 16:  # 4 is the index for 'e' and 16 is the index for 'q'
-        continue  # Skip 'e' and 'q'
-    letter = chr(97 + i)  # 97 is the ASCII code for 'a'
+    if i == 4 or i == 16:
+        continue
+    letter = chr(97 + i)
     print(letter, end='')
