e905f5861e6141feeaa6c052949abe896f24965f
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-07 15:37:54 +0200

    add add()

--- python_fundamentals/functions_modules/add.py
+++ python_fundamentals/functions_modules/add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-# Write a program that imports the function add() from the file add_0.py and prints the result of the addition 1 + 2 = 3
+
 from add_0 import add
 
-a = 1
-b = 2
-
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
