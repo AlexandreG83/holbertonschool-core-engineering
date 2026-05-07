95efb08dc3bb31b80a5aa24b9250b27b28f3ccb8
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-07 15:41:45 +0200

    ld

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
-    """Print the last digit of a number.
-
-    Args:
-        number (int): The number to extract the last digit from.
-
-    Returns:
-        int: The last digit of the number.
-    """
-    last_digit = abs(number) % 10
-    return last_digit
+    last_digit = abs(number)
+    print(last_digit % 10, end="")
+    return last_digit % 10
