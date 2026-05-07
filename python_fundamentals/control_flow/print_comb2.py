05fe9f09f85a9b4ebdcf098bf1fe002c0212d42d
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-07 14:53:03 +0200

    add shebang

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 for i in range(100):
     if i < 99:
         print(f"{i:02d}", end=", ")
--- python_fundamentals/control_flow/print_hexa.py
+++ python_fundamentals/control_flow/print_hexa.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 for i in range(99):
     print(f"{i} = {hex(i)}")
