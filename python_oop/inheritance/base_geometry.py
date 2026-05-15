04cf4d44642379d714eee26638c8e76d7c9aadf6
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 15:25:39 +0200

    update comm

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
+"""Defines a base geometry class."""
+
 
 class BaseGeometry:
     """Represents a base geometry."""
 
     def area(self):
         """compute the area of the geometry"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate that a value is a positive integer"""
+        """Validate that a value is a positive integer
+        Args:
+            name (str): The name of the parameter
+            value: The value to validate
+        Raises:
+            TypeError: If value is not an integer or is a boolean
+            ValueError: If value is less than or equal to 0
+        """
+
         if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
