acaa47edc90251fb70c9ac97fbdc572b6ba04e98
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 15:19:50 +0200

    add if not isinstance(value, int) or isinstance(value, bool):

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """integer_validator method"""
-        if type(value) is not int:
+        if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
