8ca1188cbb5b07cfa2b0a580c80c026e4ff97897
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 15:36:03 +0200

    str to conform with a square; init self.integer_validator

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -7,11 +7,12 @@ Rectangle = __import__('2-rectangle').Rectangle
 class Square(Rectangle):
     """ Square class that inherits from Rectangle. """
 
-    def __init__(self, size):
-        """ Initialize a Square instance. """
-        super().__init__(size, size)
+def __init__(self, size):
+    """ Initialize the square with size. """
+    self.integer_validator("size", size)
+    self.__size = size
+    super().__init__(size, size)
 
-    def __str__(self):
-        """ Return the string representation of the Square. """
-        return "[Square] {}/{}".format(self._Rectangle__width,
-                                       self._Rectangle__height)
+def __str__(self):
+    """ Return the string representation of the square. """
+    return "[Square] {}/{}".format(self.__size, self.__size)
