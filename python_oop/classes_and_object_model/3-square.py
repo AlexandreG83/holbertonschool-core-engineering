5efa920d0740356c8aa42cd4bc3ff2a92fa00459
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 14:16:44 +0200

    modify comm

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
-
-"""
-spam@camelot:~/$ ./3-main.py
-Area: 9
-'Square' object has no attribute 'size'
-'Square' object has no attribute '__size'
-Area: 25
-"""
+"""Square class with size and area method"""
 
 
 class Square:
