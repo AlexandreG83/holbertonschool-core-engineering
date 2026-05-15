282bc4d1ba144055e136b7b454cd7a1f72daed3d
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 15:39:39 +0200

    tab problem

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -7,12 +7,12 @@ Rectangle = __import__('2-rectangle').Rectangle
 class Square(Rectangle):
     """ Square class that inherits from Rectangle. """
 
-def __init__(self, size):
-    """ Initialize the square with size. """
-    self.integer_validator("size", size)
-    self.__size = size
-    super().__init__(size, size)
+    def __init__(self, size):
+        """ Initialize the square with size. """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-def __str__(self):
-    """ Return the string representation of the square. """
-    return "[Square] {}/{}".format(self.__size, self.__size)
+    def __str__(self):
+        """ Return the string representation of the square. """
+        return "[Square] {}/{}".format(self.__size, self.__size)
