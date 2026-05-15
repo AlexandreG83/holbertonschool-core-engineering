8b5e19104e7679479e3d1ecf0449f7d91ed3848e
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 15:43:11 +0200

    as 1-square

--- python_oop/inheritance/2-square.py
+++ python_oop/inheritance/2-square.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
+"""Defines a square class that inherits from Rectangle."""
 
-Rectangle = __import__('1-rectangle').Rectangle
+Rectangle = __import__('2-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """ Square class that inherits from Rectangle. """
 
     def __init__(self, size):
-        """ Initialize a Square instance. """
+        """ Initialize the square with size. """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
 
     def __str__(self):
-        """ Return the string representation of the Square. """
-        return "[Square] {}/{}".format(
-            self._Rectangle__width,
-            self._Rectangle__height
-        )
+        """ Return the string representation of the square. """
+        return "[Square] {}/{}".format(self.__size, self.__size)
