1a387ff62671c84ef8c8644338882fa580a10eb6
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 14:24:26 +0200

    add file

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python3
-"""Square class with size and area method"""
+#!/usr/bin/python3
+"""Square module"""
 
 
 class Square:
     """Defines a square"""
 
     def __init__(self, size):
-        """Initialize the square with a size"""
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """Initialize size"""
         self.__size = size
 
     def area(self):
-        """Calculate and return the area of the square"""
-
-        return self.__size * self.__size
+        """Return area of square"""
+        return self.__size ** 2
