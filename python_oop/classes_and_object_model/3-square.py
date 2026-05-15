78ac3c2832dd446a911965427b3dfc30f54b7116
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 14:29:58 +0200

    add

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python3
-"""Square module"""
+"""Square module.
+
+This module defines a Square class that represents a square and provides
+a method to compute its area.
+"""
 
 
 class Square:
-    """Defines a square"""
+    """Represents a square object.
 
-    def __init__(self, size):
-        """Initialize size"""
+    Attributes:
+        __size (int): Size of the square side.
+    """
+
+    def __init__(self, size=0):
+        """Initialize a Square instance.
+
+        Args:
+            size (int): Length of the square side (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is negative.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        """Return area of square"""
+        """Compute the area of the square.
+
+        Returns:
+            int: Area of the square.
+        """
         return self.__size ** 2
