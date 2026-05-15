0c9419ca29f32fb0323c9e3826a50402db3bd91a
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 15:28:06 +0200

    add comm for the module; remove area() and __str__

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""Defines a rectangle class that inherits from BaseGeometry."""
 BaseGeometry = __import__('base_geometry').BaseGeometry
 
 
@@ -12,11 +12,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        """Calculate the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return the string representation of the rectangle"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
