a2d0c4ae5abf8b9a60f27f6c20075d5129a99092
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-15 14:20:26 +0200

    add type and value error

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -6,6 +6,10 @@ class Square:
     """Defines a square"""
 
     def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
