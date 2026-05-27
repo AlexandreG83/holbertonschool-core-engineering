0dfa119e9238203f559df58c8e048fbe62a02112
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-27 04:21:21 +0200

    add comm, return

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""Module for writing and overriting a UTF-8 text file."""
+
 
 def write_file(filename="", text=""):
+    """Write a string to a UTF-8 file and return char count."""
     with open(filename , 'w', encoding="utf-8") as f:
-        f.write(text)
+        write_data = file.write(text)
+        return write_data
