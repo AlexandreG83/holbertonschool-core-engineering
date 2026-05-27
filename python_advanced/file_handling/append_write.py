c61091354c11d182779da34cb3726ccd913bdb09
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-27 04:25:56 +0200

    add comm

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Module for appending text to a UTF-8 file."""
+
 
 def append_write(filename="", text=""):
+    """Append a string to a UTF-8 file and return char count."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
 
