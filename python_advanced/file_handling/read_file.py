7b586f0f3f618ee6c28c3bbb160907011a78a7b7
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-27 04:08:12 +0200

    add comm for the def

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
+    """Read a UTF-8 text file and print its contents to stdout."""
     with open('my_file_0.txt', 'r', encoding="utf-8") as f:
         print(f.read(), end="")
