fa39a07c661df4f0f89626641b5063b9a861bad4
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-05-27 04:26:29 +0200

    pycodestyle

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -6,4 +6,3 @@ def append_write(filename="", text=""):
     """Append a string to a UTF-8 file and return char count."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
-
